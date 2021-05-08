d4ba505aac0ccf3c45cafc666bb243cca77acbdc
Esben Viskum <viskum@viskum.dk>
2021-05-08 17:40:44 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Esben Viskum on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
